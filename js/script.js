e5ca4f510464317815d272db4ae8be276606fcd5
Carlos JEG <Carlos.jeg98@gmail.com>
2022-08-11 22:41:12 -0400

    Primera entrega de proyecto final

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
-let comida = prompt("Que desea comer hoy? Tenemos: pizza, hamburguesa y sushi");
-let delivery = confirm("Deseas delivey?");
-const iva = 250;
-const deliverycost = 3000
+const productos = []
+let carrito = []
 
-switch (comida) {
-    case "pizza":
-        if (delivery){
-            console.log("Buenisimo ordenaste pizza con delivery serian: " + [7000+deliverycost+iva]);
-        }else {
-            console.log("Buenisimo ordenaste pizza serian: " + [7000+iva]);
-        }
-    break;
-    case "hamburguesa":
-        if (delivery){
-            console.log("Buenisimo ordenaste hamburguesa con delivery serian: " + [5000+deliverycost+iva]);
-        }else {
-            console.log("Buenisimo ordenaste hamburguesa serian: " + [5000+iva]);
-        }
-    break;
-    case "sushi":
-        if (delivery){
-            console.log("Buenisimo ordenaste sushi con delivery serian: " + [6000+deliverycost+iva]);
-        }else {
-            console.log("Buenisimo ordenaste sushi serian: " + [6000+iva]);
-        }
-    break;
-    default:
-        console.log("La orden no es valida");
-    break;
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre
+        this.precio = precio
+    }
+    precioFinal() {
+        return parseFloat((this.precio * 1.19).toFixed(2))
+    }
+}
+
+function agregandoProductos() {
+    productos.push(new Producto("Pizza Pepperoni" , 11000))
+    productos.push(new Producto("Pizza Hawaiana" , 11500))
+    productos.push(new Producto("Pizza Napolitana" , 12500))
+    productos.push(new Producto("Pizza Española" , 10000))
+    productos.push(new Producto("Pizza Vegetariana" , 12000))
+    productos.push(new Producto("Pizza Margarita" , 10500))
+}
+
+agregandoProductos() 
+
+function SeleccionarProductos() {
+    let pizza = prompt("Que pizza deseas comprar? - Pepperoni - Hawaiana - Napolitana - Española - Vegetariana - Margarita")
+    switch (pizza) {
+        case "Pepperoni":
+            carrito = productos.splice(0, 1)
+
+        break
+        case "Hawaiana":
+            carrito = productos.splice(1, 1)
+        break
+        case "Napolitana":
+            carrito = productos.splice(2, 1)
+        break
+        case "Española":
+            carrito = productos.splice(3, 1)
+        break
+        case "Vegetariana":
+            carrito = productos.splice(4, 1)
+        break
+        case "Margarita":
+            carrito = productos.splice(5, 1)
+        break
+        default:
+            console.log("Producto no disponible")
+    }
 }
