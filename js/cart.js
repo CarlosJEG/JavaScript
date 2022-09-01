c7e18a4fb058fdf5f30fd4f82131196613f26b7d
Carlos JEG <Carlos.jeg98@gmail.com>
2022-09-01 10:07:35 -0400

    Desafio libreria

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,27 @@
 const selectPizza = document.querySelector("#optionOne")
 const selectCost = document.querySelector("#optionTwo")
+const btnAceptar = document.querySelector("#btn-aceptar")
+const btnCancelar= document.querySelector("#btn-cancelar")
 
 selectPizza.innerText = localStorage.getItem("Nombre de la Pizza")
 selectCost.innerText += " " + localStorage.getItem("Precio de la Pizza")
+
+btnAceptar.addEventListener("click", () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Has comprado la pizza',
+        showConfirmButton: false,
+        timer: 1500
+      })
+})
+
+btnCancelar.addEventListener("click", () => {
+    Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Has cancelado el pedido',
+        showConfirmButton: false,
+        timer: 1500
+      })
+})
--- js/variables.js
+++ js/variables.js
@@ -20,3 +20,4 @@ const sizePizza = [
 ]
 
 const iva = 1.19
+
