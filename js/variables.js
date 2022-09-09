beb781a736ee62515d96facff9b58a5a0055d6f1
Carlos JEG <Carlos.jeg98@gmail.com>
2022-09-08 22:28:25 -0400

    Desafio Fetch

--- js/variables.js
+++ js/variables.js
@@ -21,3 +21,16 @@ const sizePizza = [
 
 const iva = 1.19
 
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bbcc7a67cfmshbdfdd5620579701p131a5ajsn41d66c98272f',
+		'X-RapidAPI-Host': 'movies115.p.rapidapi.com'
+	}
+};
+
+fetch('https://movies115.p.rapidapi.com/cartaz', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
